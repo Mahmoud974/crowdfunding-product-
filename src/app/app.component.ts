34b4f9b40d7106e5ec3d2924ab4e83fbdcd9d6a3
Mahmoud Zakaria <manuscrit974@gmail.com>
2024-11-15 18:21:42 +0100

    en cours

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { AmountBarComponent } from './components/amount-bar/amount-bar.component
 import { AboutComponent } from './components/about/about.component';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
+import { ConfirmDialogTemplateDemo } from './components/confirm-dialog-template-demo/confirm-dialog-template-demo.component';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -17,11 +18,52 @@ import { ButtonModule } from 'primeng/button';
     AmountBarComponent,
     AboutComponent,
     ButtonModule,
+    ConfirmDialogTemplateDemo,
+    ConfirmDialogTemplateDemo,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
+  pledgeOptions = [
+    {
+      title: 'Pledge with no reward',
+      pledge: ' ',
+      description:
+        'Choose to support us without a reward if you simply believe in our project. As a backer, you will be signed up to receive product updates via email.',
+      amount: 0,
+      buttonLabel: 'Enter your pledge',
+      price: null,
+    },
+    {
+      title: 'Bamboo Stand',
+      pledge: 'Pledge $ 25 or more',
+      description:
+        'You get a Black Special Edition computer stand and a personal thank you. You’ll be added to our Backer member list. Shipping is included.',
+      amount: 25,
+      buttonLabel: 'Continue',
+      price: 200,
+    },
+    {
+      title: 'Black Edition Stand',
+      pledge: 'Pledge $ 75 or more',
+      description:
+        'You get a Black Special Edition computer stand and a personal thank you. You’ll be added to our Backer member list. Shipping is included.',
+      amount: 75,
+      buttonLabel: 'Continue',
+      price: 200,
+    },
+    {
+      title: 'Mahogany Special Edition',
+      pledge: 'Pledge $ 200 or more',
+      description:
+        'You get two Special Edition Mahogany stands, a Backer T-Shirt, and a personal thank you. You’ll be added to our Backer member list. Shipping is included.',
+      amount: 200,
+      buttonLabel: 'Continue',
+      price: 80,
+    },
+  ];
+
   title = 'crowdfunding';
   checked: boolean = false;
 }
---
+++ src/app/components/confirm-dialog-template-demo/confirm-dialog-template-demo.component.ts
@@ -0,0 +1,75 @@
+import { Component } from '@angular/core';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ToastModule } from 'primeng/toast';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'confirm-dialog-template-demo',
+  templateUrl: './confirm-dialog-template-demo.component.html',
+  standalone: true,
+  imports: [ConfirmDialogModule, ToastModule, ButtonModule],
+  providers: [ConfirmationService, MessageService],
+})
+export class ConfirmDialogTemplateDemo {
+  constructor(
+    private confirmationService: ConfirmationService,
+    private messageService: MessageService
+  ) {
+    this.messageService.add({
+      severity: 'info',
+      summary: 'Test',
+      detail: 'Toast Test Successful',
+    });
+  }
+
+  confirm1(event: Event) {
+    this.confirmationService.confirm({
+      target: event.target as EventTarget,
+      message: 'Are you sure that you want to proceed?',
+      header: 'Confirmation',
+      icon: 'pi pi-exclamation-triangle',
+      acceptButtonStyleClass: 'p-button-text',
+      rejectButtonStyleClass: 'p-button-text',
+      accept: () => {
+        this.messageService.add({
+          severity: 'info',
+          summary: 'Confirmed',
+          detail: 'You have accepted',
+        });
+      },
+      reject: () => {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Rejected',
+          detail: 'You have rejected',
+        });
+      },
+    });
+  }
+
+  confirm2(event: Event) {
+    this.confirmationService.confirm({
+      target: event.target as EventTarget,
+      message: 'Do you want to delete this record?',
+      header: 'Delete Confirmation',
+      icon: 'pi pi-info-circle',
+      acceptButtonStyleClass: 'p-button-danger p-button-text',
+      rejectButtonStyleClass: 'p-button-text',
+      accept: () => {
+        this.messageService.add({
+          severity: 'info',
+          summary: 'Confirmed',
+          detail: 'Record deleted',
+        });
+      },
+      reject: () => {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Rejected',
+          detail: 'You have rejected',
+        });
+      },
+    });
+  }
+}
