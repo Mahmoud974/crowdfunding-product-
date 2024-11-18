4ed1fe1e6e89b1132c450a12925d4104321daa23
Mahmoud Zakaria <manuscrit974@gmail.com>
2024-11-18 16:00:06 +0100

    en cours

--- src/app/db/donation-array.ts
+++ src/app/db/donation-array.ts
@@ -4,6 +4,7 @@ export interface PledgeOptions {
   pledge: string;
   description: string;
   amount: number;
+  amountPlans: number;
   buttonLabel: string;
   price: null | number;
 }
@@ -13,6 +14,7 @@ export const pledgeOptions: PledgeOptions[] = [
     id: 1,
     title: 'Pledge with no reward',
     pledge: ' ',
+    amountPlans: 0,
     description:
       'Choose to support us without a reward if you simply believe in our project. As a backer, you will be signed up to receive product updates via email.',
     amount: 0,
@@ -23,6 +25,7 @@ export const pledgeOptions: PledgeOptions[] = [
     id: 2,
     title: 'Bamboo Stand',
     pledge: 'Pledge $ 25 or more',
+    amountPlans: 25,
     description:
       'You get a Black Special Edition computer stand and a personal thank you. You’ll be added to our Backer member list. Shipping is included.',
     amount: 25,
@@ -33,6 +36,7 @@ export const pledgeOptions: PledgeOptions[] = [
     id: 3,
     title: 'Black Edition Stand',
     pledge: 'Pledge $ 75 or more',
+    amountPlans: 75,
     description:
       'You get a Black Special Edition computer stand and a personal thank you. You’ll be added to our Backer member list. Shipping is included.',
     amount: 75,
@@ -42,6 +46,7 @@ export const pledgeOptions: PledgeOptions[] = [
   {
     id: 4,
     title: 'Mahogany Special Edition',
+    amountPlans: 200,
     pledge: 'Pledge $ 200 or more',
     description:
       'You get two Special Edition Mahogany stands, a Backer T-Shirt, and a personal thank you. You’ll be added to our Backer member list. Shipping is included.',
